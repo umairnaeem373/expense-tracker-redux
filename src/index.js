cca0936babc39b58b5191838ccc7ff0e3e76992a
umairnaeem373 <umairnaeem373@gmail.com>
2024-05-05 21:56:08 +0500

    fixed routing

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter basename='/'>
     <Provider store={store}>
     <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
